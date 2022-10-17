6f049c653beb8e8d74fc2104f61c72b402c180e0
MeVnz <ivanmuliansyah@gmail.com>
2022-10-17 20:03:06 +0700

    Day 1 bio

---
+++ Day1.java
@@ -0,0 +1,9 @@
+public class Day1 {
+    public static void main(String[] args) {
+        String nama = "IVAN MULIANSYAH.B";
+        String asal = "Bulukumba";
+        String ttl = "Bulukumba, 05 MEI 2003";
+        int umur = 19;
+        System.out.println("Nama : "+nama+"\nAsal : "+asal+"\nTempat,Tanggal Lahir : "+ttl+"\nUmur : "+umur);
+    }
+}
