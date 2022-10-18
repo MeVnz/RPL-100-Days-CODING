00ad08fb3767822951e0b1471ed355493497f671
MeVnz <ivanmuliansyah@gmail.com>
2022-10-18 19:23:18 +0700

    Days 2 Case Sensitive

---
+++ Day2.java
@@ -0,0 +1,11 @@
+public class Day2 {
+    public static void main(String[] args) {
+        //case sensitive java
+        String nama = "IVAN";
+        System.out.println(nama);
+        String Nama = "VAN";
+        System.out.println(Nama);
+        String NAMA = "IVAN";
+        System.out.println(NAMA);
+    }
+}
