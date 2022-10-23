43a4314f4b121ee25efc6b9244067655041a3014
MeVnz <ivanmuliansyah@gmail.com>
2022-10-23 20:32:43 +0700

    Calculator sederhana

---
+++ Day7.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Day7 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Masukkan angka1 : ");
+        int angka1 = sc.nextInt();
+        System.out.print("Masukkan angka2 : ");
+        int angka2 = sc.nextInt();
+        System.out.println("+,-,x,/,%");
+        String opsi = sc.next();
+        
+        if(opsi.equals("+")){
+            System.out.println(angka1 + "+" + angka2 + "=" + (angka1+angka2));
+        }
+        else if (opsi.equals("-")){
+            System.out.println(angka1 + "-" + angka2 + "=" + (angka1-angka2));
+        }
+        else if(opsi.equals("x")){
+
+            System.out.println(angka1 + "x" + angka2 + "=" + (angka1*angka2));
+        }
+        else if(opsi.equals("/")){
+
+            System.out.println(angka1 + "/" + angka2 + "=" + (angka1/angka2));
+        }
+        else if(opsi.equals("=")){
+
+            System.out.println(angka1 + "%" + angka2 + "=" + (angka1%angka2));
+        }
+    }
+}
