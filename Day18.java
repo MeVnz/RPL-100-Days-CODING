041bc24263778faf532855c3112ee4546846efb3
MeVnz <ivanmuliansyah@gmail.com>
2022-11-03 17:41:52 +0700

    Array Looping

---
+++ Day18.java
@@ -0,0 +1,15 @@
+public class Day18 {
+    public static void main(String[] args) {
+        String [] nama = new String[5];
+
+        nama[0] = "Ivan";
+        nama[1] = "Alfian";
+        nama[2] = "Azzam";
+        nama[3] = "Arman";
+        nama[4] = "Hendri";
+
+        for(int i=0;i<nama.length;i++){
+            System.out.println("Indeks ke-"+i+" = "+nama[i]);
+        }
+    }
+}
