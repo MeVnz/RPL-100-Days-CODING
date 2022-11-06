6bae82dca9c2efe300f2696f72677e7b8a87dd50
MeVnz <ivanmuliansyah@gmail.com>
2022-11-06 07:36:08 +0700

    ArrayList Integer

---
+++ Day21.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+public class Day21 {
+    public static void main(String[] args) {
+        ArrayList<Integer> angka = new ArrayList<>();
+
+        angka.add(5);
+        angka.add(5);
+        angka.add(3);
+
+        System.out.print(angka.get(0));
+        System.out.print(angka.get(1));
+        System.out.print(angka.get(2));
+    }
+}
