a06e49d7ba114b17f8250372457a45f139b14d73
MeVnz <ivanmuliansyah@gmail.com>
2022-11-06 07:35:35 +0700

    ArrayList String

---
+++ Day20.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+public class Day20 {
+    public static void main(String[] args) {
+        ArrayList<String> mhs = new ArrayList<>();
+
+        mhs.add("Ivan");
+        mhs.add("Alfian");
+        mhs.add("Azzam");
+
+        System.out.println(mhs.get(0));
+        System.out.println(mhs.get(1));
+        System.out.println(mhs.get(2));
+    }
+}
