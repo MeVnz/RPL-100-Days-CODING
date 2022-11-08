01927075144e0197e2077ee609ff42917694cc5f
MeVnz <ivanmuliansyah@gmail.com>
2022-11-08 22:08:21 +0700

    Array Multidimensi

---
+++ Day23.java
@@ -0,0 +1,10 @@
+public class Day23 {
+    public static void main(String[] args) {
+        String[][] nama = {{"Ivan","01"},{"Muliansyah","02"}};
+
+        for(int i = 0; i < nama.length; i++){
+            System.out.println("Nama : "+ nama[i][0]);
+            System.out.println("Nomor : "+nama[i][1]);
+        }
+    }
+}
