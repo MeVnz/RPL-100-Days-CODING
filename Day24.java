7278391d1c8e2afd934b8521e3ac73225082fce9
MeVnz <ivanmuliansyah@gmail.com>
2022-11-09 22:43:02 +0700

    Soal 4

---
+++ Day24.java
@@ -0,0 +1,9 @@
+public class Day24 {
+    public static void main(String[] args) {
+        int mobil = 300000000;
+        int tabungan = 5000000 * 12;
+
+        int hasil = mobil / tabungan;
+        System.out.println("Sambo perlu menabung selama "+hasil+" tahun");
+    }
+}
