67b4f22e76080d2f22d7386aaf54105bc8efd39b
MeVnz <ivanmuliansyah@gmail.com>
2022-11-24 01:16:23 +0700

    function aritmatika

---
+++ Day38.py
@@ -0,0 +1,24 @@
+def penjumlahan(angka1, angka2):
+    hasil = angka1 + angka2
+    print(hasil)
+penjumlahan(5,5)
+
+def pengurangan(angka1, angka2):
+    hasil = angka1 - angka2
+    print(hasil)
+pengurangan(5,5)
+
+def perkalian(angka1, angka2):
+    hasil = angka1 * angka2
+    print(hasil)
+perkalian(5,5)
+
+def pembagian(angka1, angka2):
+    hasil = angka1 / angka2
+    print(hasil)
+pembagian(5,5)
+
+def modulus(angka1, angka2):
+    hasil = angka1 % angka2
+    print(hasil)
+modulus(5,5)
