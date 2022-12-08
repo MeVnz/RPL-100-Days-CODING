5e48dd20fbb3ae894e37ebb0ee523b9bf1bdc7a6
MeVnz <ivanmuliansyah@gmail.com>
2022-12-08 10:49:38 +0700

    matriks

---
+++ Day53.py
@@ -0,0 +1,7 @@
+matrix = [[2,3,4],
+           [45,23,6],
+           [65,78,12]]
+for x in range(0, len(matrix)):
+    for y in range(0, len(matrix[0])):
+        print (matrix[x][y], end=' '),
+    
