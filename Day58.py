7292e7517fa4a1d87491a98abe6c08face9c29c0
MeVnz <ivanmuliansyah@gmail.com>
2022-12-13 22:37:25 +0700

    Dat58

---
+++ Day58.py
@@ -0,0 +1,7 @@
+panjangBenda = int(input())
+inchi = panjangBenda * 100000 / 25.4
+kaki = panjangBenda * 100000 / 30.48
+yard = float(panjangBenda * 100000) / 9144
+print(inchi)
+print(kaki)
+print(yard)
