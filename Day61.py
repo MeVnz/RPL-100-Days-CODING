2008716a4913312c4f98d91565e3d237174aca3a
MeVnz <ivanmuliansyah@gmail.com>
2022-12-16 22:16:42 +0700

    Dictionary

---
+++ Day61.py
@@ -0,0 +1,7 @@
+word = {1:'Learn',2:'Python',3:'TeknikInformatika'}
+que = {'Why':'Learn','what':'Python','where':'TeknikInformatika'}
+word2 = {1:'Learn','what':'Python','where':'TeknikInformatika'}
+
+print(type(word))
+print(type(que))
+print(type(word2))
