82306bd7837e80a7f93b3fc52365800d675ff1a4
MeVnz <ivanmuliansyah@gmail.com>
2022-12-23 17:28:24 +0700

    perubahan elemen dictionary

---
+++ Day66.py
@@ -0,0 +1,7 @@
+#Perubahan elemen dictionary
+word = {'activity' : 'Learn Python',
+            'youtube' : 'Fosalgo',
+            'result' : 'I can do it!'}
+word['activity']='Learn programming'
+
+print(word)
