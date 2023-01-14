1e2565b3dc3f39eb6d0bec3406b7293da29c155a
MeVnz <ivanmuliansyah@gmail.com>
2023-01-14 16:34:56 +0700

    Day89

---
+++ Day89.py
@@ -0,0 +1,9 @@
+tinggi_persegi = int(input('Input tinggi persegi: '))
+lebar_persegi = int(input('Input lebar persegi: '))
+ 
+print()
+ 
+for i in range(tinggi_persegi):
+  for j in range(lebar_persegi):
+    print(' *',end='')
+  print()
